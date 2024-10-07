e15dda3f018855f96b786b8b2358ef6e22330820
Fahmi Zain <fahmizain000@gmail.com>
2024-10-07 13:42:00 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-center py-10">
+        <h1 className="text-2xl font-bold">EL-POSTING COMING SOON.....</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
