2c48d308b08627a570d8cae01c21e9b86b6fd19b
Fahmi Zain <fahmizain000@gmail.com>
2024-10-25 21:22:01 +0700

    create custom color and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#020617',
+        primary: '#14b8a6',
+      },
+      fontFamily: {
+        roboto: 'Roboto, sans-serif',
+        monserat: 'Montserrat, sans-serif',
+        playWrite: 'Playwrite GB S',
+        jakrta: 'Jakarta Text Regular',
+      },
+    },
   },
   plugins: [],
 }
