2c2efb263c0a3c1a015a8980d0031e04809270f0
Fahmi Zain <fahmizain000@gmail.com>
2024-10-28 14:29:18 +0700

    connect api

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,39 +1,100 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import Input from "../components/Input";
 import { motion } from "framer-motion";
+import axios from "../lib/axios";
+import { useState } from "react";
 
 function Login() {
+  const [userLogin, setUserLogin] = useState({
+    email: null,
+    password: null,
+  });
+
+  const navigate = useNavigate()
+
+  const handleLoginData = (fieldname, e) => {
+    const temp = { ...userLogin };
+    temp[fieldname] = e.target.value;
+
+    setUserLogin(temp);
+  };
+
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault();
+
+    try{
+      const response = await axios.post('/auth/login', userLogin)
+      
+      if(response.status !== 200){
+        throw new Error("Failed to Login")
+      }
+
+      if(response.data){
+        localStorage.setItem("token", response.data.token)
+      }
+
+      if(localStorage.getItem("token")){
+        navigate('/home')
+      } else {
+        navigate('/')
+      }
+
+
+    } catch(err){
+      console.error(err)
+    }
+  }
+
   return (
     <section className="h-screen bg-background flex items-center justify-center px-5 flex-col gap-y-5">
       <div className="text-center max-w-md text-white">
         <motion.h1
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: .6, delay: .5 }} 
-          className="text-white text-4xl mb-3 font-playWrite">el posting</motion.h1>
+          transition={{ duration: 0.6, delay: 0.5 }}
+          className="text-white text-4xl mb-3 font-playWrite"
+        >
+          el posting
+        </motion.h1>
         <motion.p
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: .6, delay: 1 }} 
+          transition={{ duration: 0.6, delay: 1 }}
           className="text-white"
-        >El Posting adalah platform sosial media untuk berbagi foto, teks dengan teman secara mudah.</motion.p> 
+        >
+          El Posting adalah platform sosial media untuk berbagi foto, teks dengan teman secara mudah.
+        </motion.p>
       </div>
-      <motion.div 
-        initial={{ opacity: 0, }} 
-        whileInView={{ opacity: 1,}} 
-        transition={{ duration: 1, }} 
-        className="mt-2 bg-white/10 rounded-xl max-w-md py-8 px-6 w-full">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="mt-2 bg-white/10 rounded-xl max-w-md py-8 px-6 w-full"
+      >
         <div className="mb-5 text-center">
           <h1 className="font-semibold text-white text-4xl">Login</h1>
         </div>
-        <form action="">
-          <Input label="Email" placeholder="Email" />
-          <Input label="Password" placeholder="Password" />
+        <form onSubmit={handleLoginSubmit}>
+          <Input
+            onChange={(e) => handleLoginData("email", e)}
+            id="l-email"
+            label="Email"
+            placeholder="Email"
+          />
+          <Input
+            onChange={(e) => handleLoginData("password", e)}
+            id="l-password"
+            label="Password"
+            placeholder="Password"
+          />
           <Button>Login</Button>
           <p className="text-center mt-4  text-white">
             Belum punya akun?{" "}
-            <Link className="cursor-pointer ml-1" to="/register">
+            <Link
+              className="cursor-pointer ml-1"
+              to="/register"
+            >
               Buat akun
             </Link>
           </p>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,47 +1,108 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import Input from "../components/Input";
 import { motion } from "framer-motion";
+import { useState } from "react";
+import axios from "../lib/axios";
+
 
 function Register() {
+  const [users, setUsers] = useState({
+    username: null,
+    email: null,
+    password: null
+  })
+
+  const navigate = useNavigate()
+
+  const handleRegister = (fieldname, e) => {
+    const temp = {...users}
+
+    temp[fieldname] = e.target.value
+    console.log(temp)
+    setUsers(temp)
+  }
+
+  const handleSubmitRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/auth/register', users) 
+      if (response.status === 201){
+        navigate('/')
+      }
+    } catch(err){
+      console.error(err)
+    }
+  }
+
+  
+
   return (
     <section className="h-screen bg-background flex items-center justify-center px-5 flex-col gap-y-5">
       <div className="text-center max-w-md text-white">
         <motion.h1
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: .6, delay: .5 }} 
-          className="text-white text-4xl mb-3 font-playWrite">el posting</motion.h1>
+          transition={{ duration: 0.6, delay: 0.5 }}
+          className="text-white text-4xl mb-3 font-playWrite"
+        >
+          el posting
+        </motion.h1>
         <motion.p
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: .6, delay: 1 }} 
+          transition={{ duration: 0.6, delay: 1 }}
           className="text-white"
-        >El Posting adalah platform sosial media untuk berbagi foto, teks dengan teman secara mudah.</motion.p> 
+        >
+          El Posting adalah platform sosial media untuk berbagi foto, teks dengan teman secara mudah.
+        </motion.p>
       </div>
-      <motion.div 
-        initial={{ opacity: 0, }} 
-        whileInView={{ opacity: 1,}} 
-        transition={{ duration: 1, }} 
-        className="mt-2 bg-white/10 rounded-xl max-w-md py-8 px-6 w-full">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="mt-2 bg-white/10 rounded-xl max-w-md py-8 px-6 w-full"
+      >
         <div className="mb-5 text-center">
           <h1 className="font-semibold text-white text-4xl">Register</h1>
         </div>
-        <form action="">
-          <Input label="Username" placeholder="Username" />
-          <Input label="Email" placeholder="Email" />
-          <Input label="Password" placeholder="Password" />
-          <Button>Login</Button>
+        <form onSubmit={handleSubmitRegister}>
+          <Input
+            onChange={(e) => handleRegister("username", e)}
+            name="user-f"
+            id="r-username"
+            label="Username"
+            placeholder="Username"
+          />
+          <Input
+            onChange={(e) => handleRegister("email", e)}
+            name="email-f"
+            id="r-email"
+            label="Email"
+            placeholder="Email"
+          />
+          <Input
+            onChange={(e) => handleRegister("password", e)}
+            name="password-f"
+            id="r-password"
+            label="Password"
+            placeholder="Password"
+          />
+          <Button>Register</Button>
           <p className="text-center mt-4  text-white">
-            Belum punya akun?{" "}
-            <Link className="cursor-pointer ml-1" to="/">
-              Buat akun
+            Sudah punya akun?
+            <Link
+              className="cursor-pointer ml-1"
+              to="/"
+            >
+              Login
             </Link>
           </p>
         </form>
       </motion.div>
     </section>
-  )
+  );
 }
 
-export default Register
+export default Register;
