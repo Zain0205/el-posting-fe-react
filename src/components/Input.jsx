8af8fc4e988da10459012854657decc80c4c25ff
Fahmi Zain <fahmizain000@gmail.com>
2024-10-28 14:29:02 +0700

    update style

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,11 @@
-function Input({label, type, id, name, placeholder}) {
+function Input({label, id, ...props}) {
   return (
     <>
       <div className="flex flex-col text-white gap-y-2 mb-4">
-       <label className="block text-sm font-medium text-gray-300" htmlFor="email">
+       <label className="block text-sm font-medium text-gray-300" htmlFor={id}>
         {label}
         </label>
-        <input className="w-full px-4 py-2 mt-2 text-white bg-gray-700 border border-gray-600 rounded-md focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500" type={type} placeholder={placeholder} id={id} name={name} />
+        <input {...props} id={id} className="w-full px-4 py-2 mt-2 text-white bg-gray-700 border border-gray-600 rounded-md focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"/>
       </div>
     </>
   );
