6faf76a6bfd4eff3bc662e539e5c4d6119329d05
Fahmi Zain <fahmizain000@gmail.com>
2024-10-28 15:44:29 +0700

    fix style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 function Button({ children, ...props }) {
-  return <button className="w-full py-2 text-white bg-gradient-to-r from-teal-400 to-secondaryDark rounded-md hover:from-primary hover:to-secondary" {...props} >{children}</button>;
+  return <button className="w-full py-2 text-white bg-gradient-to-r from-teal-400 to-secondaryDark rounded-md hover:from-primary hover:to-secondary mt-2" {...props} >{children}</button>;
 }
 
 export default Button;
