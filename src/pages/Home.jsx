62ae97e5e783d78acf26f768320ddf61e2abc903
Fahmi Zain <fahmizain000@gmail.com>
2024-11-01 09:51:48 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Navigate } from "react-router-dom";
+import Appbar from "../components/Appbar";
 
 function Home() {
   return (
-    <div>
-      <h1>HomePage</h1>     
-    </div>
-  )
+    <>
+      {!localStorage.getItem("token") ? (
+        <Navigate to="/" />
+      ) : (
+        <section className="h-[200vh] flex items-center justify-center bg-background">
+          <Appbar />
+          <div className="h-56 w-56 bg-white"></div>
+        </section>
+      )}
+    </>
+  );
 }
 
-export default Home
+export default Home;
