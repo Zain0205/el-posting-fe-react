304235c60f01d44268106d8798dde2d370c69fa0
Fahmi Zain <fahmizain000@gmail.com>
2024-11-04 11:44:35 +0700

    create navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import NavItem from "./NavItem";
+import { LuHome, LuPlus, LuUser } from "react-icons/lu";
+import { LuLogOut } from "react-icons/lu";
+
+function Navbar() {
+  return (
+    <section className="fixed bottom-0 left-0 right-0 bg-gray-900 border-slate-400 shadow-lg flex py-3 lg:top-0 lg:w-72 lg:flex-col lg:py-5 lg:justify-between">
+      <div className="w-full">
+        <div className="hidden lg:block text-white text-center">
+          <h1 className="font-playWrite text-2xl">el posting</h1>
+          <p className="mt-2">Sosial media sederhana keren</p>
+        </div>
+        <div className="flex justify-around lg:h-1/4 lg:mt-10 w-full lg:flex-col lg:justify-normal lg:gap-y-4">
+          <NavItem to="/home" label="Home">
+            <LuHome className="text-2xl text-dark group-hover:text-primary lg:group-hover:text-white" />
+          </NavItem>
+          <NavItem to="/add-post" label="New Post">
+            <LuPlus className="text-2xl text-dark group-hover:text-primary lg:group-hover:text-white" />
+          </NavItem>
+          <NavItem label="Profile">
+            <LuUser className="text-2xl text-dark group-hover:text-primary lg:group-hover:text-white" />
+          </NavItem>
+        </div>
+      </div>
+      <div className="hidden lg:block px-5 items-center text-white">
+        <button className="text-white cursor-pointer flex gap-x-3 py-2">
+          <LuLogOut className="text-xl" />
+          <span>Logout</span>
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default Navbar;
