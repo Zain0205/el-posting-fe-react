e114dba6f87f4e7f96d9b5624b0681b4ef355f83
Fahmi Zain <fahmizain000@gmail.com>
2024-11-06 14:02:25 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,50 @@
-import React from "react";
 import { Navigate } from "react-router-dom";
-import Appbar from "../components/Appbar";
+import Navbar from "../components/Navbar";
+import Recomendation from "../components/Recomendation";
+import PostCard from "../components/PostCard";
+import { useEffect, useState } from "react";
+import axios from "../lib/axios";
 
 function Home() {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const handleGetPost = async () => {
+      try {
+        const response = await axios.get("/post/feed");
+        console.log(response.data);
+        if (response.status === 200) {
+          setPostList(response.data);
+        }
+        console.log(postList);
+      } catch (err) {
+        console.error("error");
+      }
+    };
+
+    handleGetPost();
+  }, []);
+
   return (
     <>
       {!localStorage.getItem("token") ? (
         <Navigate to="/" />
       ) : (
-        <section className="h-[200vh] flex items-center justify-center bg-background">
-          <Appbar />
-          <div className="h-56 w-56 bg-white"></div>
+        <section className="min-h-screen flex items-center bg-background lg:pl-72 lg:pr-96">
+          <Navbar />
+          <div className="min-h-screen p-5 lg:px-56 w-full">
+            <h1 className="text-white mx-auto max-w-lg text-2xl font-roboto tracking-wide">HomeFeed</h1>
+            {postList.map((post) => (
+              <PostCard
+                key={post.id}
+                content={post.content}
+                img={post.img_url}
+                username={post.username}
+                like={post.like_count}
+              />
+            ))}
+          </div>
+          <Recomendation />
         </section>
       )}
     </>
