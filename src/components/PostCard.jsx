c6c3c6cb2189dd09bb7203df6fbe45be73724b67
Fahmi Zain <fahmizain000@gmail.com>
2024-11-12 11:43:39 +0700

    Update PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,21 +1,41 @@
 import { LuHeart } from "react-icons/lu";
 import { LuMessageCircle } from "react-icons/lu";
 import defaultProfile from "../assets/images/defaultProfile.png";
+import { Link } from "react-router-dom";
+import CommentModal from "./CommentModal";
+import { useState } from "react";
+import axios from "../lib/axios";
 
-function PostCard({ content, img, username, like, profile }) {
+function PostCard({ content, img, username, like, profile, userId, postId }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // const handleGetComments = async () => {
+  //   setIsOpen(true);
+
+  //   try {
+  //     const response = await axios.get(`/comment/list/${postId}`);
+  //     setComments(response.data);
+  //     console.log(response.data);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
   return (
     <>
       {img ? (
         <div className="mx-auto max-w-lg rounded-lg mt-5">
           <div className="flex items-center justify-between py-4">
-            <div className="flex items-center">
-              <img
-                src={profile ? profile : defaultProfile}
-                alt="profile"
-                className="w-10 h-10 rounded-full"
-              />
-              <span className="ml-3 text-white font-roboto font-semibold">{username}</span>
-            </div>
+            <Link to={`/profile/${userId}`}>
+              <div className="flex items-center">
+                <img
+                  src={profile ? profile : defaultProfile}
+                  alt="profile"
+                  className="w-10 h-10 rounded-full"
+                />
+                <span className="ml-3 text-white font-roboto font-semibold">{username}</span>
+              </div>
+            </Link>
             <button className="text-white font-bold">...</button>
           </div>
 
@@ -34,10 +54,18 @@ function PostCard({ content, img, username, like, profile }) {
                   <LuHeart className="text-white text-2xl" />
                 </button>
               </div>
-              <button>
+              <button onClick={() => setIsOpen(true)}>
                 <LuMessageCircle className="text-white text-2xl" />
               </button>
             </div>
+
+            {isOpen && (
+              <CommentModal
+                img={img}
+                postId={postId}
+                setIsOpen={setIsOpen}
+              />
+            )}
 
             <div className="mt-4">
               <p className="text-white font-semibold">{username}</p>
