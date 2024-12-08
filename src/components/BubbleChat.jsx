ef600a5f0ac30f5a5c7305cf83f77d2f3b0f5d61
Fahmi Zain <fahmizain000@gmail.com>
2024-12-08 19:34:15 +0700

    Update BubbleChat.jsx

--- src/components/BubbleChat.jsx
+++ src/components/BubbleChat.jsx
@@ -1,31 +1,33 @@
 import defaultProfile from "../assets/images/defaultProfile.png";
 
-function BubbleChat({ content, username, img, isOwn }) {
+function BubbleChat({ content, sender_img, isOwn }) {
   return (
     <>
       <div className={`flex ${isOwn ? "justify-end" : "justify-start"} mb-4`}>
-        {!isOwn && (
+        {/* {!isOwn && (
           <div className="text-sm text-gray-500 mr-2">
             <img
-              src={defaultProfile}
+              src={sender_img ? `http://localhost:3000${sender_img}` : defaultProfile}
               className="h-10 w-10 rounded-full"
               alt=""
             />
           </div>
-        )}
-        <div className={`max-w-xs px-4 py-2 rounded-lg shadow-lg ${isOwn ? "bg-blue-500 text-white rounded-tr-none" : "bg-gray-200 text-gray-800 rounded-tl-none"}`}>
-          <p>{content}</p>
-          <div className="text-xs text-gray-400 mt-1 text-right">12.00</div>
+        )} */}
+        <div className={`max-w-xs break-words px-4 gap-x-3 py-1.5 overflow- rounded-xl flex shadow-lg ${isOwn ? "bg-blue-500 text-white rounded-tr-none" : "bg-gray-200 text-gray-800 rounded-tl-none"}`}>
+          <p className="text-base whitespace-pre-wrap max-w-full break-words">{content}</p>
+          <div className="flex items-end">
+            <p className="text-[8px] place-items-end text-black mt-1 text-right">12.00</p>
+          </div>
         </div>
-        {isOwn && (
+        {/* {isOwn && (
           <div className="text-sm text-gray-500 ml-2">
             <img
-              src={defaultProfile}
+              src={sender_img ? `http://localhost:3000${sender_img}` : defaultProfile}
               className="h-10 w-10 rounded-full"
               alt=""
             />
           </div>
-        )}
+        )} */}
       </div>
     </>
   );
