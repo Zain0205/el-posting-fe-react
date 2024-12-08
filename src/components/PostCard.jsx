f817091fbf2ec3eb355804a62cff445412b7e8df
Fahmi Zain <fahmizain000@gmail.com>
2024-12-08 19:34:28 +0700

    Update PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,47 +1,21 @@
 import { LuHeart } from "react-icons/lu";
 import { LuMessageCircle } from "react-icons/lu";
 import defaultProfile from "../assets/images/defaultProfile.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CommentModal from "./CommentModal";
 import { useEffect, useState } from "react";
 import { FaHeart } from "react-icons/fa";
 import axios from "../lib/axios";
 import Cookies from "js-cookie";
+import { formatTimeAgo } from "../lib/formater";
 
-const formatTimeAgo = (timestamp) => {
-  const now = Date.now();
-  const timeDifference = Math.floor((now - new Date(timestamp)) / 1000); // Time difference in seconds
-
-  if (timeDifference < 60) {
-    return `${timeDifference} second${timeDifference !== 1 ? "s" : ""} ago`;
-  } else if (timeDifference < 3600) {
-    const minutes = Math.floor(timeDifference / 60);
-    return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`;
-  } else if (timeDifference < 86400) {
-    const hours = Math.floor(timeDifference / 3600);
-    return `${hours} hour${hours !== 1 ? "s" : ""} ago`;
-  } else if (timeDifference < 604800) { // Less than a week
-    const days = Math.floor(timeDifference / 86400);
-    return `${days} day${days !== 1 ? "s" : ""} ago`;
-  } else if (timeDifference < 2419200) { // Less than a month (~28 days)
-    const weeks = Math.floor(timeDifference / 604800);
-    return `${weeks} week${weeks !== 1 ? "s" : ""} ago`;
-  } else if (timeDifference < 29030400) { // Less than a year (~12 months)
-    const months = Math.floor(timeDifference / 2419200);
-    return `${months} month${months !== 1 ? "s" : ""} ago`;
-  } else {
-    const years = Math.floor(timeDifference / 29030400); // ~12 months in a year
-    return `${years} year${years !== 1 ? "s" : ""} ago`;
-  }
-};
-
-
-function PostCard({ content, img, username, time, like, profile, userId, postId }) {
+function PostCard({ content, img, username, time, detail, like, profile, userId, postId }) {
   const [isOpen, setIsOpen] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
   const [likeCount, setLikeCount] = useState(like);
+  const [showActionModal, setShowActionModal] = useState(false);
   const id = Cookies.get("id");
-  const postOwner = userId == id
+  const postOwner = userId == id;
 
   useEffect(() => {
     handleGetLikeStatus();
@@ -94,7 +68,8 @@ function PostCard({ content, img, username, time, like, profile, userId, postId 
 
   return (
     <>
-      <div className="mx-auto max-w-lg rounded-lg mt-5">
+      <div className="mx-auto relative max-w-lg rounded-lg mt-5">
+        {showActionModal && <ActionModal postId={postId} />}
         <div className="flex items-center justify-between py-4">
           <Link to={`/profile/${userId}`}>
             <div className="flex items-center">
@@ -106,7 +81,14 @@ function PostCard({ content, img, username, time, like, profile, userId, postId 
               <span className="ml-3 text-white font-roboto font-semibold">{username}</span>
             </div>
           </Link>
-          {postOwner && <button className="text-white font-bold">...</button>}
+          {postOwner && detail && (
+            <button
+              onClick={() => setShowActionModal(!showActionModal)}
+              className="text-white font-bold"
+            >
+              ...
+            </button>
+          )}
         </div>
 
         <div className="w-full rounded-lg overflow-hidden">
@@ -138,20 +120,40 @@ function PostCard({ content, img, username, time, like, profile, userId, postId 
           <div className="mt-4">
             <p className="text-white font-semibold">{username}</p>
             <p className="text-gray-400">Liked by {likeCount} people</p>
-            <p className="text-white">{content}</p>
+            <p className="text-white whitespace-pre-wrap">{content}</p>
             <p className="text-white">Created {formatTimeAgo(time)}</p>
-          </div>
-
-          <div className="mt-3">
-            <input
-              type="text"
-              className="w-full bg-gray-800 text-white rounded-lg py-1 px-3"
-              placeholder="Add a comment..."
-            />
           </div>
         </div>
       </div>
     </>
+  );
+}
+
+function ActionModal({ postId }) {
+  const navigate = useNavigate();
+  const id = Cookies.get("id");
+
+
+  const handleDeletePost = async () => {
+    try {
+      const response = await axios.delete(`/post/delete/${postId}`);
+      if (response.status === 200) {
+        navigate(`/profile/${id}`);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="w-40 rounded-lg bg-gray-700 cursor-pointer text-white overflow-hidden right-0 top-16 absolute">
+      <div className="hover:bg-gray-800">
+        <button className="w-full p-3 text-left" onClick={handleDeletePost}>Update</button>
+      </div>
+      <div className="hover:bg-gray-800">
+        <button className="w-full p-3 text-left" onClick={handleDeletePost}>Delete</button>
+      </div>
+    </div>
   );
 }
 
