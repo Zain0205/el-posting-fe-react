2e5508f9914ec58f7330ea74385ab9d6ed57cf9d
Fahmi Zain <fahmizain000@gmail.com>
2025-01-12 00:49:16 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://103.52.115.175',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false
       },
